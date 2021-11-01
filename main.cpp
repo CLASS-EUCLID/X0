f55e3ab4af1d0ad87ac374446b57b7d036350e12
CLASS-EUCLID <93078586+CLASS-EUCLID@users.noreply.github.com>
2021-11-01 21:21:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 - - -
 */
 int endX = 0, endY = 0;
-/*bool GameOver(char a[3][3], bool& player)
+/*
+bool GameOver(char a[3][3], bool& player)
 {
 	
 	if ((a[0][0] == a[0][1] && a[0][0] == a[0][2] || a[1][0] == a[1][1] && a[1][0] == a[1][2]|| a[2][0] == a[2][1] && a[2][0] == a[2][2] || a[0][0] == a[1][0] && a[0][0] == a[2][0] || a[0][1] == a[1][1] && a[0][1] == a[2][1] || a[0][2] == a[1][2] && a[0][2] == a[2][2] || a[0][0] == a[1][1] && a[0][0] == a[2][2] || a[0][2] == a[1][1] && a[0][2] == a[2][0]))
@@ -19,22 +20,13 @@ int endX = 0, endY = 0;
 		return 0;
 }
 */
+
 int winner;
 
-bool GameOver(char a[][3], bool& player)
+bool GameOver(char a[3][3], bool& player)
 {
 	bool moves = 0;
-	for(int i =0;i<2;i++)
-		for (int j = 0; j < 2; j++)
-		{
-			if (a[i][j] == '-')
-				moves = 1;
-		}
-	if (!moves)
-	{
-		winner = 2;
-		return 1;
-	}
+	
 	if (a[0][0] == a[1][0] && a[0][0] == a[2][0])
 	{
 		if (a[0][0] == 'O')
@@ -123,6 +115,17 @@ bool GameOver(char a[][3], bool& player)
 			winner = 1;return 1;
 		}
 	}
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+		{
+			if (a[i][j] == '-')
+				moves = 1;
+		}
+	if (!moves)
+	{
+		winner = 2;
+		return 1;
+	}
 	return 0;
 }
 int i;
@@ -172,12 +175,12 @@ void Choose(char a[3][3], bool& player)
 	}
 	else if (character == 'x')
 	{
-		if (player == 1)
+		if (player == 1 && a[endY][endX]=='-')
 		{
 			a[endY][endX] = 'X';
 			player = 0;
 		}
-		else
+		else if (a[endY][endX] == '-')
 		{
 			a[endY][endX] = 'O';
 			player = 1;
